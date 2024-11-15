64714b948f72d3d913ae01ff2175ccecac743d4e
hyeon <kshyeon114@gmail.com>
2024-11-15 14:54:24 +0900

    1

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP</h1>
+      <h3>카운트 : 5</h3>
+      <img src="./image/망그러진 곰.png" alt="망그러진곰" />
+    </div>
+  );
+};
+
+export default App;
